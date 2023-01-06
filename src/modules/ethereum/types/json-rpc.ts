9efd60521ecbe177eedfd9bae3165ebe5bd56d87
CryptoBear <cxw.crypto@gmail.com>
2023-01-06 20:02:20 +0800

    feat: add ethereum/types/json-rpc

---
+++ src/modules/ethereum/types/json-rpc.ts
@@ -0,0 +1,161 @@
+// Ethereum JSON-RPC Specification
+// https://ethereum.github.io/execution-apis/api-documentation/
+
+// 字段 not spec 代表规范没有，但 geth 返回了
+
+interface ResponseWrap<T> {
+  jsonrpc: '2.0';
+  id: string | number;
+  result: T | null;
+}
+
+export interface EthereumJsonRpcRequest {
+  jsonrpc?: '2.0';
+  id?: string | number;
+  method: string;
+  params: any[];
+}
+
+export namespace EthereumJsonRpcResponse {
+  export type EthSyncing = ResponseWrap<EthereumJsonRpc.Syncing | boolean>;
+  export type EthGetBlockByNumber = ResponseWrap<EthereumJsonRpc.Block>;
+  export type EthGetTransactionByBlockNumberAndIndex = ResponseWrap<EthereumJsonRpc.Transaction>;
+  export type EthGetTransactionReceipt = ResponseWrap<EthereumJsonRpc.TransactionReceipt>;
+}
+
+export namespace EthereumJsonRpc {
+  export interface Syncing {
+    startingBlock: string;
+    currentBlock: string;
+    highestBlock: string;
+    healedBytecodeBytes?: string; // not spec
+    healedBytecodes?: string; // not spec
+    healedTrienodeBytes?: string; // not spec
+    healedTrienodes?: string; // not spec
+    healingBytecode?: string; // not spec
+    healingTrienodes?: string; // not spec
+    syncedAccountBytes?: string; // not spec
+    syncedAccounts?: string; // not spec
+    syncedBytecodeBytes?: string; // not spec
+    syncedBytecodes?: string; // not spec
+    syncedStorage?: string; // not spec
+    syncedStorageBytes?: string; // not spec
+  }
+
+  export interface Block {
+    hash: string; // not spec
+    parentHash: string;
+    sha3Uncles: string;
+    miner: string;
+    stateRoot: string;
+    transactionsRoot: string;
+    receiptsRoot: string;
+    logsBloom: string;
+    difficulty?: string;
+    number: string;
+    gasLimit: string;
+    gasUsed: string;
+    timestamp: string;
+    extraData: string;
+    mixHash: string;
+    nonce: string;
+    totalDifficulty?: string;
+    baseFeePerGas?: string;
+    size: string;
+    transactions: string[] | Transaction[];
+    uncles: string[];
+  }
+
+  export type Transaction = Transaction_legacy | Transaction_1559 | Transaction_2930;
+
+  export interface Transaction_legacy {
+    blockHash?: string; // not spec
+    blockNumber?: string; // not spec
+    transactionIndex?: string; // not spec
+    type: string;
+    nonce: string;
+    from?: string; // not spec
+    to?: string;
+    gas: string;
+    value: string;
+    input: string;
+    gasPrice: string;
+    chainId?: string;
+    v: string;
+    r: string;
+    s: string;
+  }
+
+  export interface Transaction_1559 {
+    blockHash?: string; // not spec
+    blockNumber?: string; // not spec
+    transactionIndex?: string; // not spec
+    type: string;
+    nonce: string;
+    from?: string; // not spec
+    to?: string;
+    gas: string;
+    value: string;
+    input: string;
+    maxPriorityFeePerGas: string;
+    maxFeePerGas: string;
+    accessList: TransactionAccess[];
+    chainId: string;
+    yParity: string;
+    r: string;
+    s: string;
+  }
+
+  export interface Transaction_2930 {
+    blockHash?: string; // not spec
+    blockNumber?: string; // not spec
+    transactionIndex?: string; // not spec
+    type: string;
+    nonce: string;
+    from?: string; // not spec
+    to?: string;
+    gas: string;
+    value: string;
+    input: string;
+    gasPrice: string;
+    accessList: TransactionAccess[];
+    chainId: string;
+    yParity: string;
+    r: string;
+    s: string;
+  }
+
+  export interface TransactionReceipt {
+    transactionHash: string;
+    transactionIndex: string;
+    blockHash: string;
+    blockNumber: string;
+    from: string;
+    to?: string;
+    cumulativeGasUsed: string;
+    gasUsed: string;
+    contractAddress: string | null;
+    logs: TransactionLog[];
+    logsBloom: string;
+    root?: string;
+    status?: string;
+    effectiveGasPrice: string;
+  }
+
+  export interface TransactionLog {
+    removed?: boolean;
+    logIndex?: string;
+    transactionIndex?: string;
+    transactionHash: string;
+    blockHash?: string;
+    blockNumber?: string;
+    address?: string;
+    data?: string;
+    topics: string[];
+  }
+
+  export interface TransactionAccess {
+    address: string;
+    storageKeys: string[];
+  }
+}
