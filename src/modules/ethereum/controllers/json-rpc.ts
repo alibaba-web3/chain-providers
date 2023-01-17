3dbf6d5089d0a5b99271530453ce1ace88dbaac9
CryptoBear <cxw.crypto@gmail.com>
2023-01-17 12:00:38 +0800

    feat: /ethereum/json-rpc 限制 namespace

--- src/modules/ethereum/controllers/json-rpc.ts
+++ src/modules/ethereum/controllers/json-rpc.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Response } from 'express';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { EthereumGethService } from '../services/geth';
 import { EthereumJsonRpcRequest } from '../types/json-rpc';
 
@@ -7,7 +8,11 @@ export class EthereumJsonRpcController {
   constructor(private ethereumGethService: EthereumGethService) {}
 
   @Post()
-  index(@Body() body: EthereumJsonRpcRequest) {
+  index(@Body() body: EthereumJsonRpcRequest, @Res() res: Response) {
+    const namespace = body.method.split('_')[0];
+    if (['eth', 'net', 'web3'].indexOf(namespace) === -1) {
+      return res.status(400).send(`rpc method namespace "${namespace}" not supported.`);
+    }
     return this.ethereumGethService.request(body);
   }
 }
