2271874e4ee831f5d2ab82814511de8974ecfd2f
CryptoBear <cxw.crypto@gmail.com>
2023-01-26 13:22:50 +0800

    feat: adjust constants files

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,2 @@
-export enum DingTalkConversationIds {
-  test = 'cidEp5oQwfzNQu9yC2riTDk+A==',
-  web3_genesis_team = 'cidyRgYjzGSSs/QTS0DhMTCVQ==',
-  web3_community = 'cidOFP6PFyRUDofdPK1x1PZ+g==',
-}
-
-const url = 'https://oapi.dingtalk.com/robot/send?access_token=';
-export const DingTalkBotUrls = {
-  [DingTalkConversationIds.test]: url + process.env.DINGTALK_BOT_TOKEN_TEST,
-  [DingTalkConversationIds.web3_genesis_team]: url + process.env.DINGTALK_BOT_TOKEN_WEB3_GENESIS_TEAM,
-  [DingTalkConversationIds.web3_community]: url + process.env.DINGTALK_BOT_TOKEN_WEB3_COMMUNITY,
-};
+export * from './dingtalk';
+export * from './ethereum';
