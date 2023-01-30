ebb7e75edab2bab8139c81d0ba0c68dd23b4a077
CryptoBear <cxw.crypto@gmail.com>
2023-01-30 11:45:54 +0800

    feat: add web3_community dingtalk room

--- src/constants/dingtalk.ts
+++ src/constants/dingtalk.ts
@@ -1,12 +1,14 @@
 export enum DingTalkConversationIds {
   test = 'cidEp5oQwfzNQu9yC2riTDk+A==',
   web3_genesis_team = 'cidyRgYjzGSSs/QTS0DhMTCVQ==',
-  web3_community = 'cidOFP6PFyRUDofdPK1x1PZ+g==',
+  web3_seminar = 'cidOFP6PFyRUDofdPK1x1PZ+g==',
+  web3_community = 'cidg36MXkyFtGKH84mxnWItOQ==',
 }
 
 const url = 'https://oapi.dingtalk.com/robot/send?access_token=';
 export const DingTalkBotUrls = {
   [DingTalkConversationIds.test]: url + process.env.DINGTALK_BOT_TOKEN_TEST,
   [DingTalkConversationIds.web3_genesis_team]: url + process.env.DINGTALK_BOT_TOKEN_WEB3_GENESIS_TEAM,
+  [DingTalkConversationIds.web3_seminar]: url + process.env.DINGTALK_BOT_TOKEN_WEB3_SEMINAR,
   [DingTalkConversationIds.web3_community]: url + process.env.DINGTALK_BOT_TOKEN_WEB3_COMMUNITY,
 };
