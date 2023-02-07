7389b69d31cc43dd3235b58c904be4ad5e5c702c
CryptoBear <cxw.crypto@gmail.com>
2023-02-07 17:25:10 +0800

    feat: add erc20Contracts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
-export * from './dingtalk';
-export * from './ethereum';
 export * from './env';
+export * from './dingtalk';
+export * from './ethereum/erc20';
+export * from './ethereum/sync-to-mysql';
